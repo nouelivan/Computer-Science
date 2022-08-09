93f3627036e504ccaa30e00526897cccdeec3b69
Ivan Nouel <61523959+nouelivan@users.noreply.github.com>
2022-08-09 12:54:50 -0400

    Create stacks.py

---
+++ Data Structures and Algorithms/Stacks/stacks.py
@@ -0,0 +1,66 @@
+# This program implements a stack.
+
+from node import Node
+
+# Stack Class
+class Stack:
+  def __init__(self, limit=1000):
+    self.top_item = None
+    self.size = 0
+    self.limit = limit
+  
+  # Push method
+  def push(self, value):
+    if self.has_space():
+      item = Node(value)
+      item.set_next_node(self.top_item)
+      self.top_item = item
+      self.size += 1
+      print("Adding {} to the pizza stack!".format(value))
+    else:
+      print("No room for {}!".format(value))
+
+# Pop Method
+  def pop(self):
+    if not self.is_empty():
+      item_to_remove = self.top_item
+      self.top_item = item_to_remove.get_next_node()
+      self.size -= 1
+      print("Delivering " + item_to_remove.get_value())
+      return item_to_remove.get_value()
+    print("All out of pizza.")
+
+# Peek Method
+  def peek(self):
+    if not self.is_empty():
+      return self.top_item.get_value()
+    print("Nothing to see here!")
+
+# Checks to see if there is space in stack.
+  def has_space(self):
+    return self.limit > self.size
+
+# Checks to see if the stack is empty.
+  def is_empty(self):
+    return self.size == 0
+  
+# Defining an empty pizza stack
+pizza_stack = Stack(6)
+# Adding pizzas as they are ready until we have 
+pizza_stack.push("pizza #1")
+pizza_stack.push("pizza #2")
+pizza_stack.push("pizza #3")
+pizza_stack.push("pizza #4")
+pizza_stack.push("pizza #5")
+pizza_stack.push("pizza #6")
+
+# Delivering pizzas from the top of the stack down
+print("The first pizza to deliver is " + pizza_stack.peek())
+pizza_stack.pop()
+pizza_stack.pop()
+pizza_stack.pop()
+pizza_stack.pop()
+pizza_stack.pop()
+pizza_stack.pop()
+
+pizza_stack.pop()

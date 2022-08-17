0ae92b53c754afb14e06ade82082436d528d1826
Ivan Nouel <61523959+nouelivan@users.noreply.github.com>
2022-08-17 00:03:24 -0400

    Create merge_sort.py

---
+++ Data Structures and Algorithms/Sorting Algorithms/merge_sort.py
@@ -0,0 +1,46 @@
+# This program merge sorts a list.
+
+# This function splits the list and calls the merge function.
+def merge_sort(items):
+  if len(items) <= 1:
+    return items
+
+  middle_index = len(items) // 2
+  left_split = items[:middle_index]
+  right_split = items[middle_index:]
+  left_sorted = merge_sort(left_split)
+  right_sorted = merge_sort(right_split)
+
+  return merge(left_sorted, right_sorted)
+
+# This function sorts the two halfs of the original list and creates a new sorted list.
+def merge(left, right):
+  result = []
+
+  while (left and right):
+    if left[0] < right[0]:
+      result.append(left[0])
+      left.pop(0)
+    else:
+      result.append(right[0])
+      right.pop(0)
+
+  if left:
+    result += left
+  if right:
+    result += right
+
+  return result
+
+# Unordered Lists
+unordered_list1 = [356, 746, 264, 569, 949, 895, 125, 455]
+unordered_list2 = [787, 677, 391, 318, 543, 717, 180, 113, 795, 19, 202, 534, 201, 370, 276, 975, 403, 624, 770, 595, 571, 268, 373]
+unordered_list3 = [860, 380, 151, 585, 743, 542, 147, 820, 439, 865, 924, 387]
+
+# Lists are reordered.
+ordered_list1 = merge_sort(unordered_list1)
+ordered_list2 = merge_sort(unordered_list2)
+ordered_list3 = merge_sort(unordered_list3)
+
+# Print reordered lists.
+print("{}\n\n {}\n\n {}".format(ordered_list1, ordered_list2, ordered_list3))

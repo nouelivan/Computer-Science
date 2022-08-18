b16f6cbcbfa6f1b18133490573c2da47f4b130ef
Ivan Nouel <61523959+nouelivan@users.noreply.github.com>
2022-08-18 19:15:19 -0400

    Create tree.py

---
+++ Data Structures and Algorithms/Trees/Breadth-First Search/tree.py
@@ -0,0 +1,26 @@
+# This file contains the tree implementation.
+
+from collections import deque
+
+class TreeNode:
+  def __init__(self, value):
+   self.value = value
+   self.children = []
+
+  # Creates a visual representation of the tree structure.
+  def __str__(self):
+    stack = deque()
+    stack.append([self, 0])
+    level_str = "\n"
+    while len(stack) > 0:
+      node, level = stack.pop()
+      
+      if level > 0:
+        level_str += "| "*(level-1)+ "|-"
+      level_str += str(node.value)
+      level_str += "\n"
+      level+=1
+      for child in reversed(node.children):
+        stack.append([child, level])
+
+    return level_str
